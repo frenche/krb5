ecef7f6726e5bb4bca90212140250127ed7e0c0b
Isaac Boukris <iboukris@gmail.com>
2019-03-03 14:55:56 +0200

    RBCD: Add PA-PAC-OPTIONS in constrained delegation
    to signal resource-based-constrained-delegation support.
    
    Signed-off-by: Isaac Boukris <iboukris@gmail.com>

--- src/lib/krb5/krb/get_creds.c
+++ src/lib/krb5/krb/get_creds.c
@@ -261,6 +261,7 @@ make_request(krb5_context context, krb5_tkt_creds_context ctx,
 {
     krb5_error_code code;
     krb5_data request = empty_data();
+    krb5_pa_data **in_padata = NULL;
 
     ctx->kdcopt = extra_options | FLAGS2OPTS(ctx->cur_tgt->ticket_flags);
 
@@ -275,12 +276,49 @@ make_request(krb5_context context, krb5_tkt_creds_context ctx,
     if (code)
         return code;
 
+    if (ctx->kdcopt & KDC_OPT_CNAME_IN_ADDL_TKT) {
+        krb5_pa_pac_options pac_options;
+        krb5_data *pac_options_data = NULL;
+        krb5_pa_data *padata;
+
+        in_padata = calloc(2, sizeof(krb5_pa_data *));
+        if (in_padata == NULL)
+            return ENOMEM;
+
+        padata = malloc(sizeof(*padata));
+        if (padata == NULL) {
+            krb5_free_pa_data(context, in_padata);
+            return ENOMEM;
+        }
+
+        memset(&pac_options, 0, sizeof(pac_options));
+        pac_options.options |= 0x10000000;
+
+        code = encode_krb5_pa_pac_options(&pac_options, &pac_options_data);
+        if (code != 0) {
+            krb5_free_pa_data(context, in_padata);
+            return code;
+        }
+
+        padata->magic = KV5M_PA_DATA;
+        padata->pa_type = 167;
+        padata->length = pac_options_data->length;
+        padata->contents = (krb5_octet *)pac_options_data->data;
+
+        free(pac_options_data);
+        pac_options_data = NULL;
+
+        in_padata[0] = padata;
+        in_padata[1] = NULL;
+    }
+
     krb5_free_keyblock(context, ctx->subkey);
     ctx->subkey = NULL;
     code = k5_make_tgs_req(context, ctx->fast_state, ctx->cur_tgt, ctx->kdcopt,
-                           ctx->cur_tgt->addresses, NULL, ctx->tgs_in_creds,
+                           ctx->cur_tgt->addresses, in_padata, ctx->tgs_in_creds,
                            NULL, NULL, &request, &ctx->timestamp, &ctx->nonce,
                            &ctx->subkey);
+    krb5_free_pa_data(context, in_padata);
     if (code != 0)
         return code;
 
