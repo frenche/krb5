817129729678975da412ecba1326ccc3e7c2a138
Isaac Boukris <iboukris@gmail.com>
2019-03-12 21:59:55 +0200

    RBCD: Ignore forwardable flag of evidence ticket
    With the introduction of resource-based-constrained-delegation,
    the lack of forwardable flag no longer means the ticket cannot
    be used for constrained delegation requests.
    
    Instead, we should check in the PAC if the user is sensitive,
    and error out in that case rather than making a failed request.
    But we don't always have access to the PAC and we currently do
    not have the code to retreive this attribute from the PAC.
    
    Signed-off-by: Isaac Boukris <iboukris@gmail.com>
    
    krb5: do not require keytab for constrained delegation
    
    Signed-off-by: Isaac Boukris <iboukris@gmail.com>

--- src/lib/krb5/krb/s4u_creds.c
+++ src/lib/krb5/krb/s4u_creds.c
@@ -752,25 +752,14 @@ krb5_get_credentials_for_proxy(krb5_context context,
     *out_creds = NULL;
 
     if (in_creds == NULL || in_creds->client == NULL ||
-        evidence_tkt == NULL || evidence_tkt->enc_part2 == NULL) {
+        evidence_tkt == NULL) {
         code = EINVAL;
         goto cleanup;
     }
 
     /*
-     * Caller should have set in_creds->client to match evidence
-     * ticket client
+     * Caller should set in_creds->client to match evidence ticket client.
      */
-    if (!krb5_principal_compare(context, evidence_tkt->enc_part2->client,
-                                in_creds->client)) {
-        code = EINVAL;
-        goto cleanup;
-    }
-
-    if ((evidence_tkt->enc_part2->flags & TKT_FLG_FORWARDABLE) == 0) {
-        code = KRB5_TKT_NOT_FORWARDABLE;
-        goto cleanup;
-    }
 
     code = krb5int_construct_matching_creds(context, options, in_creds,
                                             &mcreds, &fields);
@@ -817,7 +806,7 @@ krb5_get_credentials_for_proxy(krb5_context context,
      * krb5_get_credentials() (enc_part2 is unavailable in clear)
      */
     if (!krb5_principal_compare(context,
-                                evidence_tkt->enc_part2->client,
+                                in_creds->client,
                                 (*out_creds)->client)) {
         code = KRB5_KDCREP_MODIFIED;
         goto cleanup;
